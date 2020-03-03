8f11bff1cf56d5abafa8fc51d4e9f8a012c2faf5
Paul Welsh <spacedawwwg@gmail.com>
2020-03-03 16:51:41 +0000

    fix: remove purgecss

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
-const glob = require('glob');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 module.exports = {
   configureWebpack: () => {
@@ -16,12 +14,6 @@ module.exports = {
           // Required - Routes to render.
           routes: ['/', '/thanks', '/registration']
         }),
-
-        new PurgecssPlugin({
-          paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`, {
-            nodir: true
-          })
-        })
       ]
     };
   }
